ffc7d76bb7a067cfe6d24cb06d9a706d41d2065a
Alex Ruddick <a.ruddick@numat-tech.com>
2022-08-23 15:57:50 -0500

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as in_file:
 
 setup(
     name="sartorius",
-    version="0.2.2",
+    version="0.3.0",
     description="Python driver for Sartorius and Minebea Intec scales.",
     long_description=long_description,
     long_description_content_type='text/markdown',
