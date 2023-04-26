984a89bc7e23af60c63fd6a6a8cc907f00e49bc2
Alex Ruddick <a.ruddick@numat-tech.com>
2023-04-26 15:37:03 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as in_file:
 
 setup(
     name="sartorius",
-    version="0.3.1",
+    version="0.4.0",
     description="Python driver for Sartorius and Minebea Intec scales.",
     long_description=long_description,
     long_description_content_type='text/markdown',
